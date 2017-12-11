4f3f18d3e8a42c782a5b051c1ba747b40dec4c57
Morten Larsen <morten.o.larsen@bouvet.no>
2017-12-11 15:07:34 +0100

    Changed copy and compare settings to use admin share when comparing and copying

--- Copy-ServerConfigs.ps1
+++ Copy-ServerConfigs.ps1
@@ -16,8 +16,8 @@ function Copy-ServerConfigs {
 }
 
 $paths = @{
-    "C:\ChannelTemplates\" = @("..\C_ChannelTemplates\*");
-    (Join-Path -Path $Env:ProgramData -ChildPath "Mosart Medialab\ConfigurationFiles\") = @("..\C_ProgramData_MosartMedialab_ConfigurationFiles\*")
+  "C:\ChannelTemplates\" = @("..\C_ChannelTemplates\*");
+  (Join-Path -Path $Env:ProgramData -ChildPath "Mosart Medialab\ConfigurationFiles\") = @("..\C_ProgramData_MosartMedialab_ConfigurationFiles\*")
 }
 
 $paths.GetEnumerator() | Copy-ServerConfigs -Files $_.Value -Destination $_.Name
