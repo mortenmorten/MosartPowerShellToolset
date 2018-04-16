a09ca1e8d49f16f21ab88d15fc2989605877f37a
Morten Larsen <morten.o.larsen@bouvet.no>
2018-04-16 12:04:55 +0200

    Install module for all users using -AllUsers. Requires elevation.

--- Install.ps1
+++ Install.ps1
@@ -1,12 +1,35 @@
 param(
-  [switch] $Verbose = $Verbose
+  [switch] $AllUsers,
+  [switch] $Verbose = $Verbose,
+  [switch] $Confirm = $Confirm
 )
+
+# Require elevation if AllUsers
+If ($AllUsers -and !([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { 
+  Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -AllUsers" -Verb RunAs; 
+  Exit
+}
+
+Write-Host "Installing MosartPowershellTools module to $(If ($AllUsers) { "all users"} Else { "local user" })"
+While ($Confirm -and $choice -notmatch "[y|n]"){
+  $choice = read-host "Do you want to continue? (Y/N)"
+}
+
+If ($choice -eq "n") {
+  Write-Host "Installation cancelled!"
+  Exit
+}
 
 # Uninstall the module if it's already present
 Remove-Module -Name MosartPowershellToolset -ErrorAction SilentlyContinue -Verbose:$Verbose
 
 # Get the current users modules folder
-$moduleFolder = "$home\Documents\WindowsPowerShell\Modules\MosartPowershellToolset"
+$moduleFolder = "$home\Documents\WindowsPowerShell"
+If ($AllUsers) {
+  $moduleFolder = $PSHome
+}
+
+$moduleFolder += "\Modules\MosartPowershellToolset"
 
 # Delete the currently installed module
 Remove-Item -Path $moduleFolder -Force -Recurse -ErrorAction SilentlyContinue -Verbose:$Verbose | Out-Null
